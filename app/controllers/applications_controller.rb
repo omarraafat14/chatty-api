22d8b8b2f10c15f13a3edfd92885c9af235c12aa
Omar Raafat <omarraafat14@gmail.com>
2024-09-07 08:54:20 +0300

    fix parameter name at applications_controller

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -41,7 +41,7 @@ class ApplicationsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_application
-      @application = Application.find_by!(token: params[:id])
+      @application = Application.find_by!(token: params[:token])
     end
 
     # Only allow a list of trusted parameters through.
