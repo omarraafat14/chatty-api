92d8c2badbe45b3dc775cf5acfb638c79a153d58
Omar Raafat <omarraafat14@gmail.com>
2024-09-07 08:56:16 +0300

    refactor chat_controller

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,36 +1,34 @@
 class ChatsController < ApplicationController
+  before_action :set_application
   before_action :set_chat, only: %i[ show update destroy ]
 
   # GET /applications/:application_token/chats
   def index
-    @chats = Chat.all
+    @chats = @application.chats.all
 
     render json: @chats
   end
 
-  # GET /applications/:application_token/chats/:id
+  # GET /applications/:application_token/chats/:chat_number
   def show
     render json: @chat
   end
 
   # POST /applications/:application_token/chats
   def create
-    @application = Application.find_by!(token: params[:application_id])
     @chat = @application.chats.create(chat_params)
     render json: { number: @chat.number }, status: :created
   end
 
-  # PATCH/PUT /applications/:application_token/chats/:id
+  # PATCH/PUT /applications/:application_token/chats/:chat_number
   def update
-    @application = Application.find_by!(token: params[:application_id])
     @chat = @application.chats.update(chat_params)
     render json: @chat
   end
 
-  # DELETE /applications/:application_token/chats/:id
+  # DELETE /applications/:application_token/chats/:chat_number
   def destroy
-    @application = Application.find_by!(token: params[:application_id])
-    @chat = @application.chats.find(params[:id])
+    @chat = @application.chats.find_by!(number: params[:chat_number])
     @chat.destroy
 
     head :no_content
@@ -38,8 +36,12 @@ class ChatsController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
+    def set_application
+      @application = Application.find_by!(token: params[:application_token])
+    end
+
     def set_chat
-      @chat = Chat.find(params[:id])
+      @chat = Chat.find_by!(number: params[:number])
     end
 
     # Only allow a list of trusted parameters through.
