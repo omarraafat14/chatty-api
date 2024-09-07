93857a6c1b477a5c850b00d761a5badf39e0b71f
Omar Raafat <omarraafat14@gmail.com>
2024-09-07 08:57:05 +0300

    add message model

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,20 @@
 class Message < ApplicationRecord
+  # Association
+  belongs_to :chat, dependent: :destroy
+
+  # Validations
+  validates :body, presence: true
+  validates :number, presence: true, uniqueness: { scope: :chat_id }
+
+  # Callbacks
+  before_validation :set_message_number, on: :create
+
+  # Scopes
+  scope :ordered, -> { order(created_at: :desc) }
+
+  private
+  def set_message_number
+    last_message = chat.messages.order(number: :desc).first
+    self.number = last_message ? last_message.number + 1 : 1
+  end
 end
