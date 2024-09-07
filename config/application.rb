843bca778811035252975e718aeaa56f70217fcb
Omar Raafat <omarraafat14@gmail.com>
2024-09-07 18:54:17 +0300

    Setting the sidekiq queuing backend

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,8 @@ module ChattyApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Setting the queuing backend
+    config.active_job.queue_adapter = :sidekiq
   end
 end
